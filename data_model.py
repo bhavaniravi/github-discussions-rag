7d65f04badd00228d6869163a9bdf8f87dc6bd08
Bhavani Ravi <bhava0895@gmail.com>
2025-04-19 11:21:36 +0530

    chore: add embedding script

--- data_model.py
+++ data_model.py
@@ -1,13 +1,17 @@
 from typing import List, Dict
+from dataclasses import dataclass
 
+@dataclass
 class Answer:
     id: str
     ans: str
 
+@dataclass
 class Question:
     id: str
     question: str 
     answers: List[Answer]
 
+@dataclass
 class Data:
     questions: Dict[Question, List[Answer]] = None
---
+++ embeddings.py
@@ -0,0 +1,37 @@
+from data_model import Data
+from sentence_transformers import SentenceTransformer
+import numpy as np
+import textwrap
+
+class Embedding:
+    def __init__(self, data: Data):
+        self.docs_embed = None # nd array with embedding
+        self.appended_q_a_list = None # qa docs array to get the embeddings back to data
+        self.model = SentenceTransformer("Alibaba-NLP/gte-base-en-v1.5", trust_remote_code=True)
+        self.create_embedding(data)
+
+
+    def create_embedding(self, data: Data):
+        for question in data.questions:
+            answers = question.answers
+            # we are appending each question to each answer in a discussion
+            self.appended_q_a_list = ["\n".join(question, answer.ans) for answer in answers]
+            self.docs_embed = self.model.encode(self.appended_q_a_list, normalize_embeddings=True)
+
+
+    def fetch_document(self, query: str, top_n=3):
+        query_embed = self.model.encode(query, normalize_embeddings=True)
+        similarities = np.dot(self.docs_embed, query_embed.T)
+        top_idx = np.argsort(similarities, axis=0)[-top_n:][::-1].tolist()
+        most_similar_documents = [self.appended_q_a_list[idx] for idx in top_idx]
+
+        # TODO: Move this to a different function
+        CONTEXT = ""
+        for i, p in enumerate(most_similar_documents):
+            wrapped_text = textwrap.fill(p, width=100)
+
+            print("-----------------------------------------------------------------")
+            print(wrapped_text)
+            print("-----------------------------------------------------------------")
+            CONTEXT += wrapped_text + "\n\n"
+        return CONTEXT
